f757e53e1492b55cd9f0100a7b03ac324c2fa40d
Adam Sharp <adsharp@me.com>
2016-11-16 17:56:17 -0500

    Remove redundant check in throttle(while:on:)
    When `shouldThrottle.producer` is started, the observer will be executed
    synchronously first time. This makes the initial check redundant,
    because the value will be updated as soon as `shouldThrottle` is
    observed.

--- Sources/Signal.swift
+++ Sources/Signal.swift
@@ -1515,7 +1515,7 @@ extension SignalProtocol {
 		where P.Value == Bool
 	{
 		return Signal { observer in
-			let initial: ThrottleWhileState<Value> = shouldThrottle.value ? .throttled(nil) : .resumed
+			let initial: ThrottleWhileState<Value> = .resumed
 			let state = Atomic(initial)
 			let schedulerDisposable = SerialDisposable()
 
